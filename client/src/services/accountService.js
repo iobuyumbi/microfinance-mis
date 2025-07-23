d9004a753526589811565afd5afa1fc28282ada0
Innocent Obuyumbi Nyongesa <iobuyumbi@gmail.com>
2025-07-23 14:59:44 +0300

    chore: cleanup deprecated lib/axios imports and unify API usage in services

--- client/src/services/accountService.js
+++ client/src/services/accountService.js
@@ -1,4 +1,4 @@
-import api from "../lib/axios";
+import api from "./api";
 
 export const accountService = {
   getAll: (params) => api.get("/accounts", { params }).then((res) => res.data),
--- client/src/services/repaymentService.js
+++ client/src/services/repaymentService.js
@@ -1,4 +1,4 @@
-import api from "../lib/axios";
+import api from "./api";
 
 export const repaymentService = {
   getAll: (params) =>
