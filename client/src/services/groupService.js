8019cb925df38631ae22f7ef9fba88522d1d9944
iobuyumbi <iobuyumbi@gmail.com>
2025-07-28 03:30:27 +0300

    feat: add joinGroup method to groupService for group joining functionality

--- client/src/services/groupService.js
+++ client/src/services/groupService.js
@@ -1,22 +1,43 @@
-import api from './api';
-import { handleRequest } from './handleRequest';
+import api from "./api";
+import { handleRequest } from "./handleRequest";
 
 export const groupService = {
   getAll: (params) =>
-    handleRequest(() => api.get('/groups', { params }), 'Unable to load groups'),
+    handleRequest(
+      () => api.get("/groups", { params }),
+      "Unable to load groups"
+    ),
 
   getById: (id) =>
-    handleRequest(() => api.get(`/groups/${id}`), `Group with ID ${id} not found`),
+    handleRequest(
+      () => api.get(`/groups/${id}`),
+      `Group with ID ${id} not found`
+    ),
 
   create: (data) =>
-    handleRequest(() => api.post('/groups', data), 'Could not create group'),
+    handleRequest(() => api.post("/groups", data), "Could not create group"),
 
   update: (id, data) =>
-    handleRequest(() => api.put(`/groups/${id}`, data), `Could not update group ${id}`),
+    handleRequest(
+      () => api.put(`/groups/${id}`, data),
+      `Could not update group ${id}`
+    ),
 
   remove: (id) =>
-    handleRequest(() => api.delete(`/groups/${id}`), `Could not delete group ${id}`),
+    handleRequest(
+      () => api.delete(`/groups/${id}`),
+      `Could not delete group ${id}`
+    ),
 
   getUserGroups: (userId) =>
-    handleRequest(() => api.get(`/users/${userId}/groups`), `Could not load groups for user ${userId}`),
+    handleRequest(
+      () => api.get(`/users/${userId}/groups`),
+      `Could not load groups for user ${userId}`
+    ),
+
+  joinGroup: (groupId) =>
+    handleRequest(
+      () => api.post(`/groups/${groupId}/join`),
+      `Could not join group ${groupId}`
+    ),
 };
