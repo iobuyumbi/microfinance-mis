e9e910677136e15f5d49a3249c50105d1d573396
iobuyumbi <iobuyumbi@gmail.com>
2025-08-27 11:25:22 +0300

    client(services): fix financialService API import to services/api/client; update lockfile and add form validation deps

--- client/src/services/financialService.js
+++ client/src/services/financialService.js
@@ -4,7 +4,7 @@
  * Ensures reliability and maintainability across the application
  */
 
-import api from "./api/api";
+import { api } from "./api/client";
 import {
   FinancialCalculations,
   FinancialValidation,
